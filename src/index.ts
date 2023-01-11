18f1c58cfd9b81b9ec8fea9a8dcc5d141a100315
Vabian124 <82999916+Vabian124@users.noreply.github.com>
2023-01-11 12:16:30 +0100

    basic ts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+let message: string = "Hello World";
+console.log(message);
+message = "";
+const array: string[] = [];
+array.push("test");
+array.push("test2");
+array.push("test3");
+array.forEach(element => {
+    console.log(element);
+});
